f16dfe0c970bc1698a1119bf1c037c062c3af79f
SuperCat7593 <vuongtrong93@gmail.com>
2025-08-04 14:34:28 +0700

    Add files via upload

---
+++ form.js
@@ -0,0 +1,13 @@
+document.getElementById('cvForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  const data = {
+    name: this.name.value,
+    email: this.email.value,
+    phone: this.phone.value,
+    skills: this.skills.value,
+    experience: this.experience.value,
+    education: this.education.value,
+  };
+  localStorage.setItem('cvData', JSON.stringify(data));
+  window.location.href = "templates/template1-custom.html";
+});
